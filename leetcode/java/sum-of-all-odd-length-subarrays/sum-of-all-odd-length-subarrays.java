d195e1779e990643eade90655363fe0707d7704a
Pratik Rathod <89502694+pratikk-rathod@users.noreply.github.com>
2025-12-02 21:59:06 +0530

    Add solution sum-of-all-odd-length-subarrays.java (GitLeetSync)

--- leetcode/java/sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
+++ leetcode/java/sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
@@ -1,30 +1,4 @@
-class Solution:
-    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        prefix_sum=0
-        prefix_sum_array=[0]
-        ans=0
-        for i in arr:
-            prefix_sum+=i
-            prefix_sum_array.append(prefix_sum)
-        
-
-        n=len(arr)
-
-        if n%2==0:
-            n-=1
-        if n ==1 :
-            return prefix_sum_array[-1]
-        
-        while n >=1:
-            
-            for i in range(0,len(arr)):
-
-                print(i)
-                if i + n >len(arr):
-                   
-                    break
-                ans+= prefix_sum_array[i+n]
-                ans -= prefix_sum_array[i]
-            
-            n-=2
-        return ans
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> 
+int:
+        
