541ed918fe0333663e53f6d87abc2357943723ca
Pratik Rathod <89502694+pratikk-rathod@users.noreply.github.com>
2025-12-05 12:19:31 +0530

    Add solution count-partitions-with-even-sum-difference.py (GitLeetSync)

---
+++ leetcode/python/count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPartitions(self, nums: List[int]) -> int:
+        prefix_sum=[]
+        summ=0
+
+        for i in nums:
+            summ+=i
+            prefix_sum.append(summ)
+        print(prefix_sum)
+        ans=0
+        for i in range(len(prefix_sum) -1):
+            if abs(2 * prefix_sum[i] - prefix_sum[-1]) %2 ==0:
+                ans+=1
+        return ans
