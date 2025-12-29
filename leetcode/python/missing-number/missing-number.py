668305c6390df87e1bdb7a1cbc4cd0aa8123e39a
Pratik Rathod <89502694+pratikk-rathod@users.noreply.github.com>
2025-12-29 11:31:24 -0800

    Add solution missing-number.py (GitLeetSync)

--- leetcode/python/missing-number/missing-number.py
+++ leetcode/python/missing-number/missing-number.py
@@ -1,3 +1,6 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # nums.sort()
+        n =len(nums)
+        return n*(n+1) //2 -sum(nums)
+        
