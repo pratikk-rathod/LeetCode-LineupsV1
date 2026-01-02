2ddfd27f49049dc8dd6ca32f755801e4adccbfa6
Pratik Rathod <89502694+pratikk-rathod@users.noreply.github.com>
2026-01-03 01:31:55 +0530

    Add solution n-repeated-element-in-size-2n-array.py (GitLeetSync)

--- leetcode/python/n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ leetcode/python/n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,10 +1,20 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        nums.sort()
-        for i, val in enumerate(nums):
-            prev=i
-            nextt=i+1
 
-            if val==nums[nextt]:
-                return val
-        return 3
+        dictt={}
+
+        for i in nums:
+            if i in dictt:
+                return i
+            else:
+                dictt[i]=0
+    
+#         nums.sort()
+#         for i, val in enumerate(nums):
+#             prev=i
+#             nextt=i+1
+
+#             if val==nums[nextt]:
+#                 return val
+#         return 3
+
