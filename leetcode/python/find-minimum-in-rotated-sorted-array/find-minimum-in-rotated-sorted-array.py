3564a7816aac7e413e3c9254a15d93a870332c91
Pratik Rathod <89502694+pratikk-rathod@users.noreply.github.com>
2026-01-14 10:14:50 +0530

    Add solution find-minimum-in-rotated-sorted-array.py (GitLeetSync)

--- leetcode/python/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ leetcode/python/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,5 +1,36 @@
 # Brute Force
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+#         nums.sort()
+#         return nums[0]
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[0]
+
+        def binarySearch(nums,low,high,temp):
+            if high<=low:
+                return nums[low]
+            
+            mid = low + (high-low)//2
+
+            print(low,mid, high)
+
+            if nums[low]<= nums[mid] and nums[mid] <= nums[high]:
+
+                high=mid-1
+                return binarySearch(nums,low,high,temp)
+
+            elif nums[low]<= nums[mid] and nums[mid] >= nums[high]:
+                low=mid+1
+                
+                return binarySearch(nums,low,high,temp)
+            else:
+                low=low+1
+                return binarySearch(nums,low,high,temp)
+
+
+        
+
+        low,high = 0, len(nums)-1
+
+        return binarySearch(nums,low,high,0)
